fa6302f768dd83893519cf5f6a7f754cec75fa87
joanloaiza99 <joan.loaiza99@gmail.com>
2024-06-11 17:08:16 -0500

    🔨 Actualización db

--- app/database.py
+++ app/database.py
@@ -3,10 +3,10 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 import os
 
-DB_USER = os.getenv('DB_USER', 'postgres')
-DB_PASSWORD = os.getenv('DB_PASSWORD', '1234')
-DB_HOST = os.getenv('DB_HOST', 'localhost')
-DB_PORT = os.getenv('DB_PORT', '5433')
+DB_USER = os.getenv('DB_USER', 'chami')
+DB_PASSWORD = os.getenv('DB_PASSWORD', 'chami')
+DB_HOST = os.getenv('DB_HOST', '10.1.11.230')
+DB_PORT = os.getenv('DB_PORT', '5432')
 DB_NAME = os.getenv('DB_NAME', 'servomonitor')
 
 SQLALCHEMY_DATABASE_URL = f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
